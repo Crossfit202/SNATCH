42cea6ba8ec6d0892e82676fd23f5d4a37c8c4f4
Joncross202 <cro21035@byui.edu>
2025-01-16 15:24:29 -0500

    can you see application.properties?

--- heist-api/src/main/java/com/skillstorm/models/Heist.java
+++ heist-api/src/main/java/com/skillstorm/models/Heist.java
@@ -1,5 +1,34 @@
 package com.skillstorm.models;
 
-public class Heist {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "heist")
+public class Heist {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "heist_id")
+	private int heistId;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@Column(name = "location")
+	private String location;
+	
+	@Column(name = "is_assigned")
+	private boolean isAssigned; 
+	
+	@Column(name = "status")
+	private boolean status; 
+	
+	@Column(name = "crew_id")
+	private int crewId;
+	
+		
 }
