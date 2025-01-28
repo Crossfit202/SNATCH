a0ba50519eb540bd8d4f4f61b06360b0f9fb88aa
Miranda Liu <miranda.liu8@gmail.com>
2025-01-28 17:26:38 -0500

    added comments to crew service

--- personnel-management-api/src/main/java/com/skillstorm/services/CrewService.java
+++ personnel-management-api/src/main/java/com/skillstorm/services/CrewService.java
@@ -23,10 +23,17 @@ import com.skillstorm.repositories.PersonnelRepository;
 @Service
 public class CrewService {
 	private CrewRepository repo;
+	
+	// Inject HeistClient to communicate with Heist API
 	private HeistClient heistClient;
+	
+	// Inject Personnel Repo to retrieve Personnel Object (hmmm...might not need actually...)
 	private PersonnelRepository personnelRepo;
+	
+	// Inject Captain Repo to retrieve Captain Object
 	private CaptainRepository captainRepo;
 
+	// Constructor with constructor injection for repositories
 	public CrewService(CrewRepository repo, HeistClient heistClient, PersonnelRepository personnelRepo, CaptainRepository captainRepo) {
 		super();
 		this.repo = repo;
@@ -35,16 +42,19 @@ public class CrewService {
 		this.captainRepo = captainRepo;
 	}
 
-//	SERVICE METHODS
+/*
+ * SERVICE METHODS
+ */
 	
-//	GET ALL
+//	GET ALL CREW
 	public Iterable<Crew> findAll() {
 		return repo.findAll();
 	}
 	
-//	FIND BY ID
-	
+//	FIND CREW BY ID
 	public ResponseEntity<Object> findById(int crewId) {
+		
+		// Check if requested Crew exists in the DB
 		if (repo.existsById(crewId)) {
 			return ResponseEntity.status(HttpStatus.OK)
 				     			 .body(repo.findById(crewId).get());
@@ -54,10 +64,15 @@ public class CrewService {
 		}
 	}
 	
-//	GET HEIST BY CREW ID
+//	GET ASSOCIATED HEIST BY CREW ID
 	public ResponseEntity<HeistDTO> findHeistByCrewId(int crewId) {
+		
+		// Check if requested Crew exists in the DB
 		if (repo.existsById(crewId)) {
+			
+			// Use the Heist Client to retrieve associated Heist
 			return heistClient.findByCrewId(crewId);
+			
 		} else {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND)
 								 .body(null);
@@ -65,37 +80,44 @@ public class CrewService {
 	}
 	
 	
-//	CREATE ONE - POST
+//	CREATE ONE CREW - POST
 	public ResponseEntity<Object> addOne(CrewDTO crewDTO) {
 		
 		// Check if inputed Crew Name is a duplicate
 		if(repo.existsByCrewNameIgnoreCase(crewDTO.getCrewName())) {
 			return ResponseEntity.status(HttpStatus.CONFLICT)
-								 .body(String.format("Crew with name '%s' already exists!", crewDTO.getCrewName()));
+								 .body(String.format("Crew with name '%s' already exists!", 
+										 				crewDTO.getCrewName()));
 		} 
 		
 		// Check if the inputed Captain is already assigned to a Crew
 		if(crewDTO.getCaptain() != null && repo.existsByCaptain_CaptainId(crewDTO.getCaptain().getCaptainId())) {
 			return ResponseEntity.status(HttpStatus.CONFLICT)
-                    .body(String.format("Captain with ID '%d' is already assigned to a crew!", crewDTO.getCaptain().getCaptainId()));
+                    			 .body(String.format("Captain with ID '%d' is already assigned to a crew!", 
+                    					 				crewDTO.getCaptain().getCaptainId()));
 		} else {
 			
+			// Get Captain Object from input Captain ID
 			Captain captain = captainRepo.findById(crewDTO.getCaptain().getCaptainId()).orElse(null);
 			
+			// Save the new Crew with associated Captain Object
 			Crew savedCrew = repo.save(new Crew(0, crewDTO.getCrewName(),
                     							   crewDTO.getMaxCapacity(),
                     							   crewDTO.isAvailability(),
                     							   crewDTO.isHasCaptain(),
                     							   captain, 
                     							   null));
+			
+			// Retrieve the newly saved Crew Object
 			Crew completeCrew = repo.findById(savedCrew.getCrewId()).orElse(null);
 			
+			// Null handling in case the Crew did not save properly
 			if(completeCrew != null) {
 				return ResponseEntity.status(HttpStatus.CREATED)
 			             			 .body(completeCrew);
 			} else {
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-           			 .body("Error occurred while retrieving the saved Crew.");
+           			 				 .body("Error occurred while retrieving the saved Crew.");
 			}
 
 		}
@@ -103,13 +125,15 @@ public class CrewService {
 	}
 	
 	
-//	PUT
+//	UPDATE ONE CREW - PUT
 		@Transactional
 		public ResponseEntity<Object> updateOne(int crewId, CrewDTO crewDTO) {
+			
 			// Check if the Crew ID exists in DB, else quick exit (no point in doing the rest!)
 		    if(!repo.existsById(crewId)) {
 		    	return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-			             .body(String.format("Failed to update! Crew with ID %d does not exist. Ensure the ID is correct or create a new Crew.", crewId));
+			             			 .body(String.format("Failed to update! Crew with ID %d does not exist. Ensure the ID is correct or create a new Crew.", 
+			             					 				crewId));
 		    }
 		    
 		    // Get the current Crew's ID
@@ -134,17 +158,19 @@ public class CrewService {
 		    if(captainExists && crewNameExists) {
 		    	return ResponseEntity.status(HttpStatus.CONFLICT)
 		    						 .body(String.format("Failed to update! Crew with with name '%s' already exists, and Captain with ID '%d' is already assigned to a crew!", 
-		    								 crewDTO.getCrewName(), crewDTO.getCaptain().getCaptainId()));
+		    								 				crewDTO.getCrewName(), crewDTO.getCaptain().getCaptainId()));
 		    	
 		    // If only the captain selected conflicts
 		    } else if(captainExists) {
 		    	return ResponseEntity.status(HttpStatus.CONFLICT)
-		    						 .body(String.format("Failed to update! Captain with ID '%d' is already assigned to a crew!", crewDTO.getCaptain().getCaptainId()));
+		    						 .body(String.format("Failed to update! Captain with ID '%d' is already assigned to a crew!", 
+		    								 				crewDTO.getCaptain().getCaptainId()));
 		    
 		    // If only the name conflicts
 		    } else if(crewNameExists) {
 		    	return ResponseEntity.status(HttpStatus.CONFLICT)
-	                    .body(String.format("Failed to update! Crew with with name '%s' already exists!", crewDTO.getCrewName()));
+	                    			 .body(String.format("Failed to update! Crew with with name '%s' already exists!", 
+	                    									crewDTO.getCrewName()));
 		    }
 		    
 		    
@@ -152,38 +178,41 @@ public class CrewService {
 		     * PERSONNEL VALIDATION
 		     */
 		    
-		    // Check if the inputed list of Personnels exceeds the Crew's max capacity
+		    // Check if the incoming list of Personnels exceeds the Crew's max capacity
 		    if(crewDTO.getPersonnels() != null && crewDTO.getPersonnels().size() > crewDTO.getMaxCapacity()) {
+		    	
 		        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
 		                             .body(String.format("Failed to update! The number of personnel (%d) exceeds the crew's max capacity (%d).",
-		                                                 crewDTO.getPersonnels().size(), crewDTO.getMaxCapacity()));
+		                                                 	crewDTO.getPersonnels().size(), crewDTO.getMaxCapacity()));
 		    } else {
 		    	
 			    // To allow updates to the personnel list
 			    List<Integer> personnelIds = new ArrayList<>();
+			    
+			    // Add all Personnels from the incoming List to the List initialized above
 			    for (Personnel personnel : crewDTO.getPersonnels()) {
 			        personnelIds.add(personnel.getPersonnelId());
 			    }
 			    
-			    // Update the Personnel's crew field using the custom query
+			    // Update Personnels on the Crew using above List
 			    repo.updateCrewPersonnel(currentCrew.getCrewId(), personnelIds);
+			    
+			    // Remove Personnel currently on the Crew that are not in the incoming List
 			    repo.removePersonnelNotInList(personnelIds);
 			    
 			    // If no conflicts, update the Crew!
 			    return ResponseEntity.status(HttpStatus.OK)
-				             .body(repo.save(new Crew(crewId, crewDTO.getCrewName(),
-	   		                                              crewDTO.getMaxCapacity(),
-	   		                                              crewDTO.isAvailability(),
-	   		                                              crewDTO.isHasCaptain(),
-	   		                                              crewDTO.getCaptain(),
-	   		                                              currentCrew.getPersonnels())));
+				             		 .body(repo.save(new Crew(crewId, crewDTO.getCrewName(),
+	   		                                              			  crewDTO.getMaxCapacity(),
+	   		                                              			  crewDTO.isAvailability(),
+	   		                                              			  crewDTO.isHasCaptain(),
+	   		                                              			  crewDTO.getCaptain(),
+	   		                                              			  currentCrew.getPersonnels())));
 		    }
-		    
-
 		}
 	
 	
-//  DELETE
+//  DELETE ONE CREW
 		public ResponseEntity<Void> deleteById(int crewId) {
 		        repo.deleteById(crewId);  
 		        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
