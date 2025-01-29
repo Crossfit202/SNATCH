35e8f6919b94948dbc77e214e2d9f6720d90ff4e
Miranda Liu <miranda.liu8@gmail.com>
2025-01-28 20:01:26 -0500

    updated personnel service to retreive full related objects during create

--- personnel-management-api/src/main/java/com/skillstorm/services/PersonnelService.java
+++ personnel-management-api/src/main/java/com/skillstorm/services/PersonnelService.java
@@ -2,20 +2,32 @@ package com.skillstorm.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import com.skillstorm.dtos.PersonnelDTO;
+import com.skillstorm.models.Crew;
 import com.skillstorm.models.Personnel;
+import com.skillstorm.models.Skill;
+import com.skillstorm.repositories.CrewRepository;
 import com.skillstorm.repositories.PersonnelRepository;
+import com.skillstorm.repositories.SkillRepository;
 
 @Service
 public class PersonnelService {
 	private PersonnelRepository repo;
+	
+	private CrewRepository crewRepo;
+	
+	private SkillRepository skillRepo;
 
-	public PersonnelService(PersonnelRepository repo) {
+	public PersonnelService(PersonnelRepository repo, CrewRepository crewRepo, SkillRepository skillRepo) {
 		super();
 		this.repo = repo;
+		this.crewRepo = crewRepo;
+		this.skillRepo = skillRepo;
 	}
 
 	
@@ -57,13 +69,38 @@ public class PersonnelService {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
 					.body(String.format("Max capacity reached for Crew. Please assign a different Crew."));
 		} else {
+			
+			// Get Captain Object from input Captain ID (set as null otherwise)
+		    Crew crew = isAssigned ? crewRepo.findById(personnelDTO.getCrew().getCrewId()).orElse(null) : null;
+			
+			// Get List of Skill Objects from input List (Stream attempt?)
+		    // Convert list to stream
+		    List<Skill> skills = personnelDTO.getSkills().stream()
+		    		// for each skill in skills, get the full skill object by its id or else set to null (non-terminal)
+		    	    .map(skill -> skillRepo.findById(skill.getSkillId()).orElse(null))
+		    	    // filter out nulls (non-terminal)
+		    	    .filter(skill -> skill != null)
+		    	    // collects the skill objects into a list (terminal)
+		    	    .collect(Collectors.toList());
+		    
+		    // Original before trying stream
+//			List<Skill> skills = new ArrayList<>();
+//			if(personnelDTO.getSkills() != null) {
+//				for(Skill skill : personnelDTO.getSkills()) {
+//					Skill fullSkill = skillRepo.findById(skill.getSkillId()).orElse(null);
+//					if(fullSkill != null) {
+//						skills.add(fullSkill);
+//					}
+//				}
+//			}
+			
 			return ResponseEntity.status(HttpStatus.CREATED)
 					.body(repo.save(new Personnel(0, personnelDTO.getPersonnelName(),
 							personnelDTO.getSpecies(),
 							personnelDTO.getProfileImg(),
 							isAssigned,
-							personnelDTO.getCrew(),
-							personnelDTO.getSkills())));
+							crew,
+							skills)));
 		}
 	}
 
