85f78f9ebbbc96872aa6228dc9c32c0ba28ca591
Miranda Liu <miranda.liu8@gmail.com>
2025-01-31 10:31:32 -0500

    changed client dto in heist

--- heist-api/src/main/java/com/skillstorm/dtos/CrewDTO.java
+++ heist-api/src/main/java/com/skillstorm/dtos/CrewDTO.java
@@ -8,18 +8,12 @@ public class CrewDTO {
 	private int maxCapacity;
 	
 	private boolean availability;
-	
-	private boolean hasCaptain;
-	
-	private int captainId;
 
-	public CrewDTO(String crewName, int maxCapacity, boolean availability, boolean hasCaptain, int captainId) {
+	public CrewDTO(String crewName, int maxCapacity, boolean availability) {
 		super();
 		this.crewName = crewName;
 		this.maxCapacity = maxCapacity;
 		this.availability = availability;
-		this.hasCaptain = hasCaptain;
-		this.captainId = captainId;
 	}
 
 	public String getCrewName() {
@@ -46,28 +40,11 @@ public class CrewDTO {
 		this.availability = availability;
 	}
 
-	public boolean isHasCaptain() {
-		return hasCaptain;
-	}
-
-	public void setHasCaptain(boolean hasCaptain) {
-		this.hasCaptain = hasCaptain;
-	}
-
-	public int getCaptainId() {
-		return captainId;
-	}
-
-	public void setCaptainId(int captainId) {
-		this.captainId = captainId;
-	}
-
 	@Override
 	public String toString() {
 		return "CrewDTO [crewName=" + crewName + ", maxCapacity=" + maxCapacity + ", availability=" + availability
-				+ ", hasCaptain=" + hasCaptain + ", captainId=" + captainId + "]";
+				+ "]";
 	}
-	
 	
 	
 
